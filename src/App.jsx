4dbe5032bebfa86cfd391b56282a0309485cb1a7
osamaMsaeed <60467335+osamaMsaeed@users.noreply.github.com>
2022-01-25 22:14:32 +0500

    initial commit
    retrieved product by ID from blockchain

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState, useContext } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { ProductsContext } from "./context/ProductsContext";
+
+function App() {
+  const { value } = useContext(ProductsContext);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+
+        <p>
+          Edit <code>App.jsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {" | "}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/utils/constants.js
@@ -0,0 +1,8 @@
+import abi from "./Products.json";
+
+export const contractABI = abi.abi;
+export const contractAddress = "0x69C0d375DC1C0441d499CDFab9ac3F4E4E804C8e";
+export const PRIVATE_KEY =
+  "0xde2f99c987ce939b0bf1a079baf0d3820f897ce96463505f1d1c7ac5eeb23552";
+//Ropsten newtork Alchemy API KEY
+export const API_KEY = "GjiW-7dRfpYNWpPFixvBpHQjljCO8M2C";
